de4dc6adf7a08f26eedee9b630dc9d41fc2c57bd
HaseebKhanzad5432 <haseebkhan.igcc@gmail.com>
2026-08-01 22:20:26 +0500

    Add Git commands for initial setup and feature branch

---
+++ Week 2 task 6.py
@@ -0,0 +1,8 @@
+git init
+git add hello.py
+git commit -m "Initial commit"
+git checkout -b feature-update
+git add hello.py
+git commit -m "Updated hello.py"
+git remote add origin https://github.com/HaseebKhanzad5432/GitPractice.git
+git push -u origin feature-update
